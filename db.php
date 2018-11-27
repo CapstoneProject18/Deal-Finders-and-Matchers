8eef33acd1c45662e1e8b7e2fa8e38daea318f19
Shobhit Kumar Dutta <32891115+shobhitkdutta@users.noreply.github.com>
2018-11-28 02:15:32 +0530

    Re-Uploading
    Updated some minor codes and segmentation issues

--- db.php
+++ db.php
@@ -5,8 +5,3 @@ if (!$connection) {
     die('Not connected : ' . mysqli_connect_error());
 }
 
-// Sets the active MySQL database.
-/*$db_selected = mysqli_select_db($connection,'accounts');
-if (!$db_selected) {
-    die ('Can\'t use db : ' . mysqli_error($connection));
-}*/
