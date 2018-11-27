b7d2417d732b182eebe9e228a3b41d21f40ec455
RahulJonnala <32276313+RahulJonnala@users.noreply.github.com>
2018-11-28 03:03:07 +0530

    Admin-Map Front End Designing Part
    Contains the basic implementation of the front end part for the admin side.

---
+++ admin-map1.php
@@ -0,0 +1,30 @@
+<?php
+include_once 'header.php';
+include_once 'locations_model.php';
+?>
+
+
+<div id="map"></div>
+
+<!------ Include the above in your HEAD tag ---------->
+<script>
+</script>
+
+<div style="display: none" id="form">
+    <table class="map1">
+        <tr>
+            <input name="id" type='hidden' id='id'/>
+            <td><a>Description:</a></td>
+            <td><textarea disabled id='description' placeholder='Description'></textarea></td>
+        </tr>
+        <tr>
+            <td><b>Confirm Location ?:</b></td>
+            <td><input id='confirmed' type='checkbox' name='confirmed'></td>
+        </tr>
+
+        <tr><td></td><td><input type='button' value='Save' onclick='saveData()'/></td></tr>
+    </table>
+</div>
+<script async defer
+        src="https://maps.googleapis.com/maps/api/js?language=en&key=AIzaSyA3Izhy-nMsv0obB8VlWg05ROu3cCs03k4&callback=initMap">
+</script>
