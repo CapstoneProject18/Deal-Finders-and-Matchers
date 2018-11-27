667ecd244a0b95b77dad8effcddf8d93e35d5f51
Shobhit Kumar Dutta <32891115+shobhitkdutta@users.noreply.github.com>
2018-11-28 02:18:11 +0530

    Database Connection Part
    *Improved the segmentation of the code
    *Removed all the comments part of the code

--- db.php
+++ db.php
@@ -1,5 +1,4 @@
 <?php
-// Opens a connection to a MySQL server.
 $connection=mysqli_connect ("localhost", 'root', '','demo');
 if (!$connection) {
     die('Not connected : ' . mysqli_connect_error());
