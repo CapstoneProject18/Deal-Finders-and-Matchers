4d0107ce0fed5b9b24befccec24921ddb1a88246
RahulJonnala <32276313+RahulJonnala@users.noreply.github.com>
2018-11-28 03:01:55 +0530

    User-Map Front End Designing Part
    Kindly find the implementation of the map with the basic of the front end coding.

---
+++ user-map1.php
@@ -0,0 +1,22 @@
+<?php
+include_once 'header.php';
+include 'locations_model.php';
+//get_unconfirmed_locations();exit;
+?>
+
+    <script type="text/javascript"
+            src="https://maps.googleapis.com/maps/api/js?language=en&key=AIzaSyA3Izhy-nMsv0obB8VlWg05ROu3cCs03k4">
+    </script>
+
+    <div id="map"></div>
+    <script>
+	</script>
+
+
+
+
+
+<?php
+include_once 'footer.php';
+
+?>
