21d8b0a5cada2c2a5c5fa720fbc8648cb55e6887
Shobhit Kumar Dutta <32891115+shobhitkdutta@users.noreply.github.com>
2018-12-19 17:33:47 +0530

    Add files via upload
    # Deal Finder and Locator
    
    A drop-in module for a website that shows a google map with list of stores in the sidebar. 
    
    ![Gif](/demo.gif?raw=true)
    
    ## Features
    - draws all deal on a map
    - shows a list of deal in the side bar providing map navigation
    - determines user location and sorts the deal, showing nearest first
    - shows a distance to each store form a given location (walking or driving)
    - allows to search for nearest store to a given location
    - provides a direction link to each store
    - allows to set custom markers
    
    ## CDN
    
    ```html
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/microapps/store-locator@1.1.1/dist/store-locator.css">
    <script src="https://cdn.jsdelivr.net/gh/microapps/store-locator@1.1.1/dist/store-locator.js"></script>
    ```
    
    ## Usage
    
    - Include [store-locator.js](/dist/store-locator.js?raw=true) and [store-locator.css](/dist/store-locator.css?raw=true) on your page
    - Add a container where you want the map to be rendered
    - Initialize the script
    
    
    ```html
    <div id="my-store-locator">
          <!-- map will be rendered here-->
    </div>
    <script src="store-locator.js"></script>
    <script>
      storeLocator({
        container: 'store-locator',
        apiKey: 'GOOGLE_MAPS_API_KEY',
        center: {lat: 42.382387, lng: -71.116648},
        travelMode: 'WALKING',
        unitSystem: 'METRIC',
        storeMarkerIcon: './storeIcon.png',
        homeMarkerIcon: './homeIcon.png',
        markerIconSize: [40, 62],
        limit: 1,
        searchHint: "Not all deal sell our whole range so if you're looking for a specific product we recommend you call ahead.",
        deal: [
          {
            name: 'Cafe Coffee Day',
            address: 'C # 1, 2,3 PP Tower, Netaji Subhash Place, Pitampura, Delhi, 110034',
            location: {lat: 28.695090, lng: 77.130710},
            website: 'https://www.cafecoffeeday.com'
          },
          ]
      })
    </script>
    ```
    
    ## Configuration options
    
    - `container` - id of the element where the map will be rendered
    - `stores` - an array of stores to render on a map,
    - `zoom` - initial map zoom `default: 6`,
    - `center` - initial map center `default: {lat: 20.0526342, lng: 64.4351598}`
    - `storeMarkerIcon` - custom store marker icon
    - `homeMarkerIcon` - custom current location marker icon
    - `markerIconSize` - an array of [x, y] to scale marker icon
    - `searchHint` - text rendered after a search input
    - `travelMode` - the mode used to calculate distance `WALKING` or `DRIVING`, `default: 'DRIVING'`
    - `unitSystem` - used to show distance `METRIC` or `IMPERIAL` `default: 'METRIC'`
    - `limit` - shows only first n closest results to the location `default: 10`
    - `homeLocationHint` - text that appears in an info window of home location marker `default: 'Current location'`
    - `farAwayMarkerOpacity` - an opacity of a marker that is too far away (determined by `limit`)
    - `fullWidthMap` - changes the appearance to make the map full width and store list as an overlay on top of it

--- README.md
+++ README.md
@@ -1,52 +1,75 @@
+# Deal Finder and Locator
 
-# Deal-Finders-and-Matchers
-Deals finders and matchers Creates a map based deal locator. Merchants in a particular area can describe a set of deals that are available. A user can then search for deals within a specific radius, The search can be further optimized by category. Categories are merchant defined and therefore a mechanism of cross-referencing the same categories described using different words becomes necessary. This uses a map based front-end and locates deals within a specific radius.
+A drop-in module for a website that shows a google map with list of stores in the sidebar. 
 
-# Getting Started
+![Gif](/demo.gif?raw=true)
 
-This instructions will help get you a copy of the project up and running on your local machine.
+## Features
+- draws all deal on a map
+- shows a list of deal in the side bar providing map navigation
+- determines user location and sorts the deal, showing nearest first
+- shows a distance to each store form a given location (walking or driving)
+- allows to search for nearest store to a given location
+- provides a direction link to each store
+- allows to set custom markers
 
-# Prerequisites
+## CDN
 
-Install Xampp
+```html
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/microapps/store-locator@1.1.1/dist/store-locator.css">
+<script src="https://cdn.jsdelivr.net/gh/microapps/store-locator@1.1.1/dist/store-locator.js"></script>
+```
 
-https://www.apachefriends.org/index.html
+## Usage
 
-Install editor       //Can use other also, Atom,Sublime text etc....
+- Include [store-locator.js](/dist/store-locator.js?raw=true) and [store-locator.css](/dist/store-locator.css?raw=true) on your page
+- Add a container where you want the map to be rendered
+- Initialize the script
 
-Notepad++
 
-https://notepad-plus-plus.org/download/v7.6.html
+```html
+<div id="my-store-locator">
+      <!-- map will be rendered here-->
+</div>
+<script src="store-locator.js"></script>
+<script>
+  storeLocator({
+    container: 'store-locator',
+    apiKey: 'GOOGLE_MAPS_API_KEY',
+    center: {lat: 42.382387, lng: -71.116648},
+    travelMode: 'WALKING',
+    unitSystem: 'METRIC',
+    storeMarkerIcon: './storeIcon.png',
+    homeMarkerIcon: './homeIcon.png',
+    markerIconSize: [40, 62],
+    limit: 1,
+    searchHint: "Not all deal sell our whole range so if you're looking for a specific product we recommend you call ahead.",
+    deal: [
+      {
+        name: 'Cafe Coffee Day',
+        address: 'C # 1, 2,3 PP Tower, Netaji Subhash Place, Pitampura, Delhi, 110034',
+        location: {lat: 28.695090, lng: 77.130710},
+        website: 'https://www.cafecoffeeday.com'
+      },
+      ]
+  })
+</script>
+```
 
-Google Map API Key Generation:
+## Configuration options
 
-https://developers.google.com/maps/documentation/
+- `container` - id of the element where the map will be rendered
+- `stores` - an array of stores to render on a map,
+- `zoom` - initial map zoom `default: 6`,
+- `center` - initial map center `default: {lat: 20.0526342, lng: 64.4351598}`
+- `storeMarkerIcon` - custom store marker icon
+- `homeMarkerIcon` - custom current location marker icon
+- `markerIconSize` - an array of [x, y] to scale marker icon
+- `searchHint` - text rendered after a search input
+- `travelMode` - the mode used to calculate distance `WALKING` or `DRIVING`, `default: 'DRIVING'`
+- `unitSystem` - used to show distance `METRIC` or `IMPERIAL` `default: 'METRIC'`
+- `limit` - shows only first n closest results to the location `default: 10`
+- `homeLocationHint` - text that appears in an info window of home location marker `default: 'Current location'`
+- `farAwayMarkerOpacity` - an opacity of a marker that is too far away (determined by `limit`)
+- `fullWidthMap` - changes the appearance to make the map full width and store list as an overlay on top of it
 
-# Running Test
-
-    -With PhpMyAdmin -- Htdocs -- LocalHost/..
-
-# Database
-
-  -Create Database Demo -- phpMySql
-  
-#  Built With
-  -Xampp Server/WAMP Server
-  
-  -Php and JavaScript
-  
-  -HTML
-  
-  -CSS
-  
-  -Google Map API and Google Places API
-  
-# Tools Used
-  -Notepad++
-  
-  -Sublime Text Editor
-  
-  -Code Writer(Windows Store Application)
-  
-  
-  
---
+++ src/components/StoreLocator.js
@@ -0,0 +1,308 @@
+import promiseMap from 'p-map';
+import cx from 'classnames';
+import {getUserLocation, loadScript} from 'lib/utils';
+import {Component} from 'preact';
+import DirectionIcon from './DirectionIcon';
+import SearchIcon from './SearchIcon';
+import classNames from './StoreLocator.css';
+import WebIcon from './WebIcon';
+
+const travelModes = {
+  DRIVING: 'car',
+  WALKING: 'walk'
+};
+
+const units = {
+  METRIC: 0,
+  IMPERIAL: 1
+};
+
+const toMiles = 1.609;
+
+class StoreLocator extends Component {
+  static defaultProps = {
+    stores: [],
+    zoom: 6,
+    limit: 10,
+    center: {lat: 20.593683, lng: 78.962883},
+    travelMode: 'DRIVING',
+    homeLocationHint: 'Current location',
+    homeMarkerIcon: 'http://maps.google.com/mapfiles/kml/pushpin/grn-pushpin.png',
+    storeMarkerIcon: 'http://maps.google.com/mapfiles/kml/pushpin/red-pushpin.png',
+    unitSystem: 'METRIC',
+    farAwayMarkerOpacity: 0.6,
+    fullWidthMap: false
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchLocation: null,
+      activeStoreId: null,
+      stores: props.stores
+    };
+    this.markers = [];
+  }
+
+  loadGoogleMaps() {
+    if (window.google && window.google.maps) return Promise.resolve();
+    return loadScript(
+      `https://maps.googleapis.com/maps/api/js?key=${this.props.apiKey}&libraries=geometry,places`
+    );
+  }
+
+  getMarkerIcon(icon) {
+    if (!icon) return null;
+    const {markerIconSize} = this.props;
+    if (typeof icon === 'string' && markerIconSize) {
+      const iconSize = markerIconSize;
+      return {
+        url: icon,
+        scaledSize: new google.maps.Size(iconSize[0], iconSize[1])
+      };
+    }
+    return icon;
+  }
+
+  addStoreMarker = store => {
+    const infoWindow = new google.maps.InfoWindow({
+      content: `<div class="${classNames.infoWindow}">
+          <h4>${store.name}</h4>
+          ${store.address}
+        </div>`
+    });
+
+    const marker = new google.maps.Marker({
+      position: store.location,
+      title: store.name,
+      map: this.map,
+      icon: this.getMarkerIcon(this.props.storeMarkerIcon)
+    });
+    marker.addListener('click', () => {
+      if (this.infoWindow) {
+        this.infoWindow.close();
+      }
+      infoWindow.open(this.map, marker);
+      this.infoWindow = infoWindow;
+      this.setState({activeStoreId: store.id});
+    });
+    this.markers.push(marker);
+    return marker;
+  };
+
+  getDistance(p1, p2) {
+    const origin = new google.maps.LatLng(p1);
+    const destination = new google.maps.LatLng(p2);
+    const directDistance = this.getDirectDistance(origin, destination);
+    return new Promise(resolve => {
+      this.distanceService.getDistanceMatrix(
+        {
+          origins: [origin],
+          destinations: [destination],
+          travelMode: this.props.travelMode,
+          unitSystem: units[this.props.unitSystem],
+          durationInTraffic: true,
+          avoidHighways: false,
+          avoidTolls: false
+        },
+        (response, status) => {
+          if (status !== 'OK') return resolve(directDistance);
+          const route = response.rows[0].elements[0];
+          if (route.status !== 'OK') return resolve(directDistance);
+          resolve({
+            distance: route.distance.value,
+            distanceText: route.distance.text,
+            durationText: route.duration.text
+          });
+        }
+      );
+    });
+  }
+
+  getDirectDistance(origin, destination) {
+    const distance =
+      google.maps.geometry.spherical.computeDistanceBetween(origin, destination) / 1000;
+    if (units[this.props.unitSystem] === 1) {
+      return {
+        distance: distance / toMiles,
+        distanceText: `${(distance / toMiles).toFixed(2)} mi`
+      };
+    }
+    return {
+      distance,
+      distanceText: `${distance.toFixed(2)} km`
+    };
+  }
+
+  setHomeMarker(location) {
+    if (this.homeMarker) {
+      this.homeMarker.setMap(null);
+    }
+    const infoWindow = new google.maps.InfoWindow({
+      content: this.props.homeLocationHint
+    });
+    this.homeMarker = new google.maps.Marker({
+      position: location,
+      title: this.props.homeLocationHint,
+      map: this.map,
+      icon: this.getMarkerIcon(this.props.homeMarkerIcon)
+    });
+    this.homeMarker.addListener('click', () => {
+      if (this.infoWindow) {
+        this.infoWindow.close();
+      }
+      infoWindow.open(this.map, this.homeMarker);
+      this.infoWindow = infoWindow;
+    });
+  }
+
+  setupMap = () => {
+    const {center, zoom} = this.props;
+    this.map = new window.google.maps.Map(this.mapFrame, {
+      center,
+      zoom,
+      mapTypeControl: false,
+      streetViewControl: false,
+      fullscreenControl: false
+    });
+    this.distanceService = new google.maps.DistanceMatrixService();
+    const geocoder = new google.maps.Geocoder();
+    this.setupAutocomplete();
+    this.state.stores.map(this.addStoreMarker);
+    getUserLocation().then(location => {
+      this.setState({searchLocation: location});
+      this.calculateDistance(location);
+      this.map.setCenter(location);
+      this.map.setZoom(11);
+      this.setHomeMarker(location);
+      geocoder.geocode({location: location}, (results, status) => {
+        if (status === 'OK') {
+          if (results[0]) {
+            this.input.value = results[0].formatted_address;
+          }
+        }
+      });
+    });
+  };
+
+  setupAutocomplete() {
+    const autocomplete = new google.maps.places.Autocomplete(this.input);
+    autocomplete.bindTo('bounds', this.map);
+    autocomplete.addListener('place_changed', () => {
+      const place = autocomplete.getPlace();
+      if (!place.geometry) return;
+
+      // If the place has a geometry, then present it on a map.
+      if (place.geometry.viewport) {
+        this.map.fitBounds(place.geometry.viewport);
+      } else {
+        this.map.setCenter(place.geometry.location);
+        this.map.setZoom(11);
+      }
+      const location = place.geometry.location.toJSON();
+      this.setState({searchLocation: location});
+      this.setHomeMarker(location);
+      this.calculateDistance(location);
+    });
+  }
+
+  clearMarkers() {
+    this.markers.forEach(m => {
+      m.setMap(null);
+    });
+    this.markers = [];
+  }
+
+  calculateDistance(searchLocation) {
+    const {stores, limit} = this.props;
+    if (!searchLocation) return stores;
+    promiseMap(stores, store => {
+      return this.getDistance(searchLocation, store.location).then(result => {
+        Object.assign(store, result);
+        return store;
+      });
+    }).then(data => {
+      let result = data.sort((a, b) => a.distance - b.distance);
+      const bounds = new google.maps.LatLngBounds();
+      bounds.extend(searchLocation);
+      this.clearMarkers();
+      result = result.map((store, i) => {
+        store.hidden = i + 1 > limit;
+        const marker = this.addStoreMarker(store);
+        if (store.hidden) {
+          marker.setOpacity(this.props.farAwayMarkerOpacity);
+        } else {
+          bounds.extend(store.location);
+        }
+        return store;
+      });
+      this.map.fitBounds(bounds);
+      this.map.setCenter(bounds.getCenter(), this.map.getZoom() - 1);
+      this.setState({stores: result});
+    });
+  }
+
+  componentDidMount() {
+    this.loadGoogleMaps().then(this.setupMap);
+  }
+
+  onStoreClick({location, id}) {
+    this.map.setCenter(location);
+    this.map.setZoom(16);
+    this.setState({activeStoreId: id});
+  }
+
+  //noinspection JSCheckFunctionSignatures
+  render({searchHint, travelMode, fullWidthMap}, {activeStoreId, stores}) {
+    return (
+      <div className={cx(classNames.container, {[classNames.fullWidthMap]: fullWidthMap})}>
+        <div className={classNames.searchBox}>
+          <div className={classNames.searchInput}>
+            <input type="text" ref={input => (this.input = input)} />
+            <SearchIcon className={classNames.searchIcon} />
+          </div>
+          {searchHint && <div className={classNames.searchHint}>{searchHint}</div>}
+          <ul className={classNames.storesList}>
+            {stores.map(store => {
+              const locationStr = `${store.location.lat},${store.location.lng}`;
+              return (
+                <li
+                  key={store.id}
+                  onClick={() => this.onStoreClick(store)}
+                  className={cx({
+                    [classNames.activeStore]: store.id === activeStoreId,
+                    [classNames.hiddenStore]: store.hidden
+                  })}>
+                  <h4>{store.name}</h4>
+                  {store.distanceText && (
+                    <div className={classNames.storeDistance}>
+                      {store.distanceText} away{' '}
+                      {store.durationText &&
+                        `(${store.durationText} by ${travelModes[travelMode]})`}
+                    </div>
+                  )}
+                  <address>{store.address}</address>
+                  <div className={classNames.storeActions} onClick={e => e.stopPropagation()}>
+                    <a target="_blank" href={`https://www.google.com/maps?daddr=@${locationStr}`}>
+                      <DirectionIcon />
+                      directions
+                    </a>{' '}
+                    {store.website && (
+                      <a target="_blank" href={store.website}>
+                        <WebIcon />
+                        website
+                      </a>
+                    )}
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className={classNames.map} ref={mapFrame => (this.mapFrame = mapFrame)} />
+      </div>
+    );
+  }
+}
+
+export default StoreLocator;
